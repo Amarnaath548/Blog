12f6a382e5af89c9480a605df8acd04624f7ac9e
Amarnaath <balajipandian.708@gmail.com>
2025-11-28 18:21:36 +0530

    fix: remove default values for Cloudinary configuration in cloudinary.js

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary=require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "dnespxsij" ,
-  api_key: process.env.CLOUDINARY_API_KEY|| "412351853219318" ,
-  api_secret: process.env.CLOUDINARY_API_SECRET || "T1QJgJPgHkHyu_F8Ixu-zRKEWwU" ,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME  ,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET  ,
 });
 
 module.exports=cloudinary
