6f96c17ef96b524fd7c6b12689e7e76125160cec
Amarnaath <balajipandian.708@gmail.com>
2025-11-28 18:33:56 +0530

    fix: update API base URL to use production endpoint

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API= axios.create({baseURL:'http://localhost:5000/api/'});
+const API= axios.create({baseURL:'https://blog-886j.onrender.com/api'});
 
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem('token');
